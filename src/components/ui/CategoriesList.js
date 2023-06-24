ee9e21098639d758a7e48c9b6399daa65d7629b4
Morad <nimomor5@gmail.com>
2023-06-24 12:37:48 +0100

    edit header and grid

--- src/components/ui/CategoriesList.js
+++ src/components/ui/CategoriesList.js
@@ -8,47 +8,52 @@ const CategoriesList = ({ setCategoriesPosition }) => {
     []
   );
   return (
-    <div className="projects-categories">
-      <div className="categorie" ref={posRef}>
-        <div className="cat-image">
-          <img
-            src="https://i.ibb.co/qkt4tCx/609acf3b7a299c11d4b136c9-ui-ux-design-tools.jpg"
-            alt=""
-          />
-          <div className="cat-name">
-            <NavLink to="/design">Design </NavLink>
-          </div>
-          <div className="cat-desc">
-            deep knowledge of ux/ui fundamentals and advanced design concepts
-            such as(sketching, wireframing, prototyping...).
-          </div>
-        </div>
-      </div>
-      <div className="categorie">
-        <div className="cat-image">
-          <img
-            src="https://i.ibb.co/99QrJyw/frontend-security-vulnerabilities-min.jpg"
-            alt=""
-          />
-          <div className="cat-name">
-            <NavLink to="/front">Front end</NavLink>
-          </div>
-          <div className="cat-desc">
-            solid front end fundamentals(html, css, sass, boostrab, javascript,
-            react...) ,most of my projects build with react but i can work with
-            other frameworks as well.
+    <div className="container">
+      <div className="projects-categories">
+        <div className="categorie" ref={posRef}>
+          <div className="cat-image">
+            <img
+              src="https://i.ibb.co/qkt4tCx/609acf3b7a299c11d4b136c9-ui-ux-design-tools.jpg"
+              alt=""
+            />
+            <div className="cat-name">
+              <NavLink to="/design">Design </NavLink>
+            </div>
+            <div className="cat-desc">
+              deep knowledge of ux/ui fundamentals and advanced design concepts
+              such as(sketching, wireframing, prototyping...).
+            </div>
           </div>
         </div>
-      </div>
-      <div className="categorie">
-        <div className="cat-image">
-          <img src="https://i.ibb.co/H7MfbKk/how-to-start-a-blog.png" alt="" />
-          <div className="cat-name">
-            <NavLink to="/blogs">Blogs </NavLink>
+        <div className="categorie">
+          <div className="cat-image">
+            <img
+              src="https://i.ibb.co/99QrJyw/frontend-security-vulnerabilities-min.jpg"
+              alt=""
+            />
+            <div className="cat-name">
+              <NavLink to="/front">Front end</NavLink>
+            </div>
+            <div className="cat-desc">
+              solid front end fundamentals(html, css, sass, boostrab, wordpress,
+              react...) ,most of my projects build with react but i can work
+              with other frameworks as well.
+            </div>
           </div>
-          <div className="cat-desc">
-            love sharing my knowledge with the community and here is few blogs
-            where i talk about code and design.
+        </div>
+        <div className="categorie">
+          <div className="cat-image">
+            <img
+              src="https://i.ibb.co/H7MfbKk/how-to-start-a-blog.png"
+              alt=""
+            />
+            <div className="cat-name">
+              <NavLink to="/blogs">Blogs </NavLink>
+            </div>
+            <div className="cat-desc">
+              love sharing my knowledge with the community and here is few blogs
+              where i talk about code and design.
+            </div>
           </div>
         </div>
       </div>
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,14 +2,22 @@ import React from "react";
 const Header = ({ scrollToCategories }) => {
   return (
     <header>
-      <div className="header-content">
+      <div
+        className="header-content"
+        style={{
+          width: "90%",
+          margin: "auto",
+        }}
+      >
         <div className="avatar-img">
           <img src="https://i.ibb.co/RYnZkV4/Ellipse-1.png" alt="profile" />
           <h3 className="hi">Hi im Morad Elazyzy</h3>
           <h2>im front end developer and ux/ui designer </h2>
           <p>
-            very interested in modern technologies, my focus is to create apps
-            that look clean and coded clean.
+            experienced UX/UI developer proficient in both front-end development
+            and design. With a passion for crafting intuitive user experiences,
+            I bring together the best of both worlds to create impactful digital
+            solutions.
           </p>
           <button className="main-btn" onClick={scrollToCategories}>
             View work
